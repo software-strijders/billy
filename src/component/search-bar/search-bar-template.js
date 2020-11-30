ae97415bce0429f472d55d4783ad2e81fd13f301
JortWillemsen <jort@maurice-nienke.nl>
2020-11-30 23:36:30 +0100

    Modify search-bar

--- src/component/search-bar/search-bar-template.js
+++ src/component/search-bar/search-bar-template.js
@@ -3,9 +3,9 @@ export const template = /*html*/ `
     @import '../component/search-bar/search-bar.css';
 </style>
 <div class="wrapper">
-  <div class="searchBar">
-    <input aria-label="zoek" placeholder="Wat wil je weten?" class="searchBar__input" type="text">
-    <button  name="zoek" class="searchBar__button" type="submit">-></button>
+  <div class="searchBar searchBar--contrast">
+    <input aria-label="zoek" placeholder="Wat wil je weten?" class="searchBar__input input--contrast" type="text">
+    <button  name="zoek" class="searchBar__button" type="submit"><img class="searchBar__arrow" src="../assets/arrow-right.svg" alt="Search arrow"/></button>
   </div>
   <a class="searchBar__link link--show" href="https://www.un.org/en/sections/issues-depth/climate-change/">Meer weten over klimaatverandering? 🌱</a>
 </div>
