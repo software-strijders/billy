a71e53a9fcf3ad195c205a426712399652564165
JortWillemsen <jort@maurice-nienke.nl>
2020-12-01 11:36:14 +0100

    Add login-button component

---
+++ src/component/login-button/login-button-template.js
@@ -0,0 +1,8 @@
+export const template = /* html */ `
+<style>
+    @import '../component/login-button/login-button.css';
+</style>
+<button class="loginButton" onclick="">
+    <h1 class="loginButton__text">Login</h1>
+</button>
+`;
