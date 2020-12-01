c4675f21cdd02f68ed1e0f61127576a634273399
JustMilan <milandol321@gmail.com>
2020-12-02 00:38:45 +0100

    Add result item component

--- src/component/result_item/result_item.js
+++ src/component/result_item/result_item.js
@@ -1,3 +1,15 @@
-const template = require('./result_item_template.js');
+import {template} from "./result_item_template.js";
 
-console.log(template)
+class result_item extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback() {
+        this._shadowRoot.innerHTML = template;
+        // this._shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+window.customElements.define("result-item", result_item);
--- src/component/result_item/result_item_template.js
+++ src/component/result_item/result_item_template.js
@@ -1,18 +1,12 @@
-template = `
-    <div class="result">
-        <div>
-            <h2>
-                Title
-            </h2>
-            <p>
-                Description <br>
-                Description <br>
-            </p>
-            <div>
-                tijd: %.f min - laatst gewijzigd; %s
-            </div>
-        </div>
-    </div>
-`
-
-modules.exports.template = {template: template};
+export const template = /*html*/ `
+<div class="result">
+  <div>
+    <h2>Title</h2>
+    <p>
+      Description <br/>
+      Description <br/>
+    </p>
+    <div>tijd: %.f min - laatst gewijzigd; %s</div>
+  </div>
+</div>
+`;
