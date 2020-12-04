7816760d51027469cde04ad6dab4f7ddf8b3604c
Xander Vedder <xander.vedd@gmail.com>
2020-12-04 20:52:28 +0100

    Refactor side-bar to use LitElement

--- src/component/side-bar/side-bar.js
+++ src/component/side-bar/side-bar.js
@@ -1,13 +1,41 @@
-import { template } from "./side-bar-template.js";
+import { LitElement, html, css } from "lit-element";
 
-class SideBar extends HTMLElement {
-  constructor() {
-    super();
-    this._shadowRoot = this.attachShadow({ mode: "open" });
+class SideBar extends LitElement {
+  static getStyles() {
+    return css`
+      @import "../component/side-bar/side-bar.css";
+    `;
   }
 
-  connectedCallback() {
-    this._shadowRoot.innerHTML = template;
+  render() {
+    return html`
+        <div class="sideBar">
+          <a href="./analyse.html" class="item">
+            <img class="item__image" src="./assets/analyse-icon.svg" />
+            Analyse
+          </a>
+
+          <a href="./advice.html" class="item">
+            <img class="item__image" src="./assets/advice-icon.svg" />
+            Advies
+          </a>
+
+          <a href="./design.html" class="item">
+            <img class="item__image" src="./assets/design-icon.svg" />
+            Ontwerp
+          </a>
+
+          <a href="./realize.html" class="item">
+            <img class="item__image" src="./assets/realize-icon.svg" />
+            Realisatie
+          </a>
+
+          <a href="./manage.html" class="item">
+            <img class="item__image" src="./assets/manage-icon.svg" />
+            Beheer
+          </a>
+        </div>
+    `;
   }
 }
 
