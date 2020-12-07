eee14126235dbd6a6facd406ae56bcef69c09b48
Xander Vedder <xander.vedd@gmail.com>
2020-12-07 19:00:03 +0100

    Add js import file
    Potentially some optimizations could be done here, but this is fine
    for now.

---
+++ src/component/components.js
@@ -0,0 +1,14 @@
+import "./app/app.js";
+import "./article/article.js";
+import "./aside/aside.js";
+import "./category-option/category-option.js";
+import "./contrast-toggle/contrast-toggle.js";
+import "./hero/hero.js";
+import "./layout/full-page-layout/full-page-layout.js";
+import "./layout/side-bar-layout/side-bar-layout.js";
+import "./login-button/login-button.js";
+import "./login-window/login-window.js";
+import "./result-item/result-item.js";
+import "./search-bar/search-bar.js";
+import "./side-bar/side-bar.js";
+import "./top-bar/top-bar.js";
