8bb441ff45105a0c125d315a20eb3cd526080e40
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2020-12-18 13:37:03 +0100

    Add state for login redux

---
+++ src/js/login.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    loggedIn: false,
+    user: {
+        email: "",
+    }
+};
+
+export const { actions, reducer } = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    login: (state, action) => { 
+        console.log(action.payload); //should be removed but should be there for demonstration purposes ~xander vedder 2020
+        return action.payload; 
+    }
+  },
+});
