4e0076abfa06bf661b1b69f3c5dd5432133b0711
JortWillemsen <jort@maurice-nienke.nl>
2021-01-07 12:27:56 +0100

    Add simple hover animation for result item

--- src/component/result-item/result-item.js
+++ src/component/result-item/result-item.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from "lit-element";
+import { classMap } from "lit-html/directives/class-map";
 
 class ResultItem extends LitElement {
   static get properties() {
@@ -16,11 +17,47 @@ class ResultItem extends LitElement {
   static getStyles() {
     return css`
       :host {
+        position: relative;
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         margin: 25px 0;
         width: 100%;
         height: auto;
+        transition: 0.3s;
+        border-radius: 5px;
+      }
+
+      :host(:hover) {
+        background-color: var(--billy-color-background-light-grey)
+      }
+
+      :host(:hover) .line {
+        min-width: 5px;
+        margin: 0 10px 0 0;
+        transform: translateX(0);
+        opacity: 1;
+      }
+
+      :host(:hover) .resultItem {
+        transform: translateX(15px);
+        
+      }
+
+      .line {
+        position: absolute;
+        height: 100%;
+        min-width: 3px;
+        background-color: var(--billy-color-line-light);
+        border-radius: 10px;
+        transform: translateX(-5px);
+        transition: 0.1s;
+        opacity: 0;
+      }
+
+      .resultItem {
+        transition: 0.1s;
+        padding: 5px;
+        border-radius: 5px;
       }
 
       .resultItem__link {
@@ -37,6 +74,7 @@ class ResultItem extends LitElement {
         color: var(--billy-color-text-secondary-dark);
         margin: 0;
         height: auto;
+        width: 98%;
         overflow: hidden;
         display: -webkit-box;
         -webkit-line-clamp: 3;
@@ -53,6 +91,8 @@ class ResultItem extends LitElement {
 
       .resultItem__category {
         background-color: var(--billy-color-background-grey);
+        color: var(--billy-color-text-primary-dark);
+        text-decoration: none;
         border-radius: 20px;
         margin: 0 10px 0 0;
         padding: 5px 10px;
@@ -64,19 +104,22 @@ class ResultItem extends LitElement {
 
   render() {
     return html`
-      <div class="resultItem__categories">
-        <p class="resultItem__category">${this.headCategory}</p>
-        <p class="resultItem__category">${this.subCategory}</p>
-      </div>
-      <a class="resultItem__link" href="${this.href}">
-        <h2 class="resultItem__title">${this.title}</h2>
-        <p class="resultItem__description">
-          <small>${this.description} </small>
-        </p>
-        <div class="resultItem__info">
-          tijd: ${this.readTime} min - laatst gewijzigd: ${this.lastRevised}
+      <div class="line"></div>
+      <div class="resultItem">
+        <div class="resultItem__categories">
+          <p class="resultItem__category">${this.headCategory}</p>
+          <p class="resultItem__category">${this.subCategory}</p>
         </div>
-      </a>
+        <a class="resultItem__link" href="${this.href}">
+          <h2 class="resultItem__title">${this.title}</h2>
+          <p class="resultItem__description">
+            <small>${this.description} </small>
+          </p>
+          <div class="resultItem__info">
+            tijd: ${this.readTime} min - laatst gewijzigd: ${this.lastRevised}
+          </div>
+        </a>
+      </div>
     `;
   }
 }
