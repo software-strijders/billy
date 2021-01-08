ba8fab4f4aaeb48b7c421c736c3f74f5f80a25fe
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-01-08 14:43:52 +0100

    Show tags when no results are found on result page

--- src/component/result-item/no-result.js
+++ src/component/result-item/no-result.js
@@ -41,14 +41,51 @@ class NoResultItem extends LitElement {
         -webkit-line-clamp: 3;
         -webkit-box-orient: vertical;
       }
+
+      .resultItem__categories {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        height: 40px;
+        justify-content: flex-start;
+      }
+
+      .resultItem__category {
+        background-color: var(--billy-color-background-grey);
+        color: var(--billy-color-text-primary-dark);
+        text-decoration: none;
+        border-radius: 20px;
+        margin: 0 10px 0 0;
+        padding: 5px 10px;
+        cursor: select;
+        font-size: 15px;
+      }
     `;
   }
 
   render() {
     return html`
+      <div class="resultItem__categories">
+          <a class="resultItem__category" href="/search?hc=${this.getHeadCategory()}">${this.getHeadCategory()}</a>
+          <a class="resultItem__category" href="/search?sc=${this.getSubCategory()}">${this.getSubCategory()}</a>
+      </div>
       <h2 class="noResultItem__title">404</h2>
       <p class="noResultItem__description">No results found</p>
     `;
+  }
+
+  getHeadCategory() {
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("hc")) {
+      return urlParams.get("hc");
+    }
+  }
+
+  getSubCategory() {
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("sc")) {
+      return urlParams.get("sc");
+    }
   }
 }
 
