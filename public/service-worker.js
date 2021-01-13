9fa80c5f0b597d26cafb2dd9c386e11336a21497
JortWillemsen <jort@maurice-nienke.nl>
2021-01-13 16:18:57 +0100

    Rework service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,19 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
+const cacheName = "Temporas";
 
-workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.CacheFirst()
-);
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    }),
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+      .then((response) => {
+        return response || fetch(event.request);
+      }),
+  );
+});
