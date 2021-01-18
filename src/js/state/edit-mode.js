09f0689faf3e465c152f57cd96f1f8ec67116070
Arjen Norbart <arjennorbart@gmail.com>
2021-01-18 13:23:08 +0100

    Fix pr comment

--- src/js/state/edit-mode.js
+++ src/js/state/edit-mode.js
@@ -1,18 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    inEditMode: false,
-    articleTitle: "",
-    articleContent: "",
+  inEditMode: false,
+  articleTitle: "",
+  articleContent: "",
 };
 
 export const { actions, reducer } = createSlice({
-    name: "editMode",
-    initialState,
-    reducers: {
-      articleToEdit: (state, action) => {
-        return action.payload;
-      },
+  name: "editMode",
+  initialState,
+  reducers: {
+    articleToEdit: (state, action) => {
+      return action.payload;
     },
-  });
-  
+  },
+});
